45932f7b1cc110099d1c36d62b591ee52ff90954
aeginidae <aeginidae@gmail.com>
2019-04-02 17:27:51 -0500

    make adjustments to account for change to createWerker

--- test/db.spec.js
+++ test/db.spec.js
@@ -21,20 +21,6 @@ const examples = {
     lat: 40.1,
     long: 40.2,
     address: '1111 St. Charles Avenue, New Orleans, LA 70115',
-    certifications: [
-      {
-        cert_name: 'hello',
-        url_Photo: 'image.jpg',
-      },
-    ],
-    positions: [
-      {
-        position: 'server',
-      },
-      {
-        position: 'bartender',
-      },
-    ],
   },
 
   otherWerker: {
@@ -58,7 +44,7 @@ const examples = {
       {
         cert_name: 'safeserv',
         url_Photo: 'safeserv.bmp',
-      }
+      },
     ],
     positions: [
       {
@@ -140,6 +126,9 @@ describe('Database', () => {
       truncate: true,
       cascade: true,
     })));
+    await dbHelpers.models.sequelize.sync({
+      force: true,
+    });
   });
   describe('Werker', () => {
     describe('addWerker', () => {
@@ -148,10 +137,9 @@ describe('Database', () => {
         expect(dbHelpers.addWerker).toBeInstanceOf(Function);
       });
       test('should create a werker', async () => {
-        const werker = await dbHelpers.addWerker(examples.Werker);
-        newWerker = werker.dataValues;
+        newWerker = await dbHelpers.addWerker(examples.Werker);
         expect(newWerker).toBeDefined();
-        expect(werker).toBeInstanceOf(dbHelpers.models.Werker);
+        expect(newWerker).toBeInstanceOf(dbHelpers.models.Werker);
       });
       [
         'id',
@@ -161,180 +149,119 @@ describe('Database', () => {
         'name_last',
         'email',
         'url_photo',
-        'bio',
         'phone',
-        'last_minute',
-        'lat',
-        'long',
-        'address',
-        'cache_rating',
-        'createdAt',
-        'updatedAt',
       ].forEach(prop => test(`should have property ${prop}`, () => {
-        expect(newWerker[prop]).toBeDefined();
+        expect(newWerker.get(prop)).toBeDefined();
       }));
-      describe('should create join table instances', () => {
-        describe('certifications', () => {
-          let certs;
-          let cert;
-          let WerkerCertification;
-          beforeAll(async () => {
-            certs = await dbHelpers.models.Certification.findAll({
-              include: [
-                {
-                  model: dbHelpers.models.Werker,
-                  through: {
-                    attributes: ['WerkerId', 'url_Photo', 'CertificationId'],
-                    where: {
-                      WerkerId: newWerker.id,
-                    },
-                  },
-                },
-              ],
-            });
-            [cert] = certs;
-            WerkerCertification = cert.Werkers[0].WerkerCertification.dataValues;
-          });
-          test('should exist', () => {
-            expect(certs).toBeDefined();
-            expect(certs[0]).toBeInstanceOf(dbHelpers.models.Certification);
-          });
-          test('should have one element', () => {
-            expect(certs.length).toEqual(1);
-          });
-          test('should have the property cert_name', () => {
-            expect(cert.cert_name).toBe(examples.Werker.certifications[0].cert_name);
-          });
-          describe('WerkerCertification', () => {
-            test('should share WerkerId with Werker', () => {
-              expect(WerkerCertification.WerkerId).toEqual(newWerker.id);
-            });
-            test('should share CertificationId with certification', () => {
-              expect(WerkerCertification.CertificationId).toEqual(cert.id);
-            });
-            test('should have the property url_Photo', () => {
-              expect(WerkerCertification.url_Photo)
-                .toEqual(examples.Werker.certifications[0].url_Photo);
-            });
-          });
+      describe('updateWerker', () => {
+        let updatedWerker;
+        test('should exist', () => {
+          expect(dbHelpers.updateWerker).toBeDefined();
+          expect(dbHelpers.updateWerker).toBeInstanceOf(Function);
         });
-        describe('positions', () => {
-          let positions;
-          let position;
-          let WerkerPosition;
-          beforeAll(async () => {
-            positions = await dbHelpers.models.Position.findAll({
-              include: [
-                {
-                  model: dbHelpers.models.Werker,
-                  through: {
-                    attributes: ['WerkerId', 'PositionId'],
-                    where: {
-                      WerkerId: newWerker.id,
-                    },
-                  },
-                },
-              ],
-            });
-            [position] = positions;
-            WerkerPosition = position.Werkers[0].WerkerPosition.dataValues;
-          });
-          test('should exist', () => {
-            expect(positions).toBeDefined();
-            expect(positions[0]).toBeInstanceOf(dbHelpers.models.Position);
-          });
-          test('should have two positions', () => {
-            expect(positions.length).toEqual(2);
-          });
-          test('should have attribute position', () => {
-            expect(['server', 'bartender']).toContain(position.position);
-          });
-          describe('WerkerPosition', () => {
-            test('should share WerkerId with werker', () => {
-              expect(WerkerPosition.WerkerId).toEqual(newWerker.id);
-            });
-            test('should share PositionId with position', () => {
-              expect(WerkerPosition.PositionId).toEqual(position.id);
-            });
-          });
-        });
-      });
-    });
-    describe('updateWerker', () => {
-      let updatedWerker;
-      test('should exist', () => {
-        expect(dbHelpers.updateWerker).toBeDefined();
-        expect(dbHelpers.updateWerker).toBeInstanceOf(Function);
-      });
-      test('should update a werker', async () => {
-        updatedWerker = await dbHelpers.updateWerker(newWerker.id, {
-          email: 'hot@mail.com',
-          certifications: [
-            {
-              cert_name: 'safeserv',
-              url_Photo: 'fake',
-            }, {
-              cert_name: 'trash man',
-              url_Photo: 'real cert',
-            },
-          ],
-          positions: [
-            {
-              position: 'dishwasher',
-            },
-          ],
-        });
-        expect(updatedWerker).toBeDefined();
-        expect(updatedWerker).not.toEqual(newWerker);
-      });
-      test('should change properties', () => {
-        expect(newWerker.email).not.toEqual(updatedWerker.email);
-      });
-      test('should not change properties', () => {
-        [
-          'name_first',
-          'name_last',
-          'url_photo',
-          'bio',
-          'access_token',
-          'refresh_token',
-          'phone',
-          'id',
-        ].forEach(prop => expect(updatedWerker[prop]).toEqual(newWerker[prop]));
-      });
-      test('should add new certifications', async () => {
-        const certs = await dbHelpers.models.Certification.findAll({
-          include: [
-            {
-              model: dbHelpers.models.Werker,
-              through: {
-                model: dbHelpers.models.WerkerCertification,
-                attributes: ['url_Photo'],
+        test('should update a werker', async () => {
+          updatedWerker = await dbHelpers.updateWerker(newWerker.id, {
+            email: 'hot@mail.com',
+            certifications: [
+              {
+                cert_name: 'safeserv',
+                url_Photo: 'fake',
+              }, {
+                cert_name: 'trash man',
+                url_Photo: 'real cert',
               },
-              where: {
-                id: updatedWerker.id,
+            ],
+            positions: [
+              {
+                position: 'dishwasher',
               },
-            },
-          ],
+            ],
+          });
+          expect(updatedWerker).toBeDefined();
+          expect(updatedWerker).not.toEqual(newWerker);
         });
-        expect(certs.some(cert => cert.get('cert_name') === 'trash man')).toBeTruthy();
-        expect(certs.some(cert => cert.dataValues.Werkers[0].WerkerCertification.dataValues.url_Photo === 'real cert')).toBeTruthy();
-      });
-      test('should add new positions', async () => {
-        const positions = await dbHelpers.models.Position.findAll({
-          include: [
-            {
-              model: dbHelpers.models.Werker,
-              through: {
-                model: dbHelpers.models.WerkerPosition,
-                where: {
-                  WerkerId: updatedWerker.id,
-                },
-              },
-            },
-          ],
+        test('should change properties', () => {
+          expect(newWerker.email).not.toEqual(updatedWerker.email);
         });
-        expect(positions.some(position => position.get('position') === 'dishwasher')).toBeTruthy();
+        test('should not change properties', () => {
+          [
+            'name_first',
+            'name_last',
+            'url_photo',
+            'bio',
+            'access_token',
+            'refresh_token',
+            'phone',
+            'id',
+          ].forEach(prop => expect(updatedWerker[prop]).toEqual(newWerker[prop]));
+        });
+        describe('should create join table instances', () => {
+          describe('certifications', () => {
+            let certs;
+            let cert;
+            let WerkerCertification;
+            beforeAll(async () => {
+              certs = await updatedWerker.getCertifications({
+                joinTableAttributes: ['url_Photo', 'WerkerId', 'CertificationId'],
+              });
+              console.log(certs);
+              [cert] = certs;
+              // eslint-disable-next-line prefer-destructuring
+              WerkerCertification = cert.WerkerCertification;
+            });
+            test('should exist', () => {
+              expect(cert).toBeInstanceOf(dbHelpers.models.Certification);
+            });
+            test('should have one element per certification added to the new werker', () => {
+              expect(certs.length).toEqual(2);
+            });
+            test('should have the property cert_name', () => {
+              expect(['safeserv', 'trash man']).toContain(cert.get('cert_name'));
+            });
+            describe('WerkerCertification', () => {
+              test('should share WerkerId with Werker', () => {
+                expect(WerkerCertification.get('WerkerId')).toEqual(newWerker.get('id'));
+              });
+              test('should share CertificationId with certification', () => {
+                expect(WerkerCertification.get('CertificationId')).toEqual(cert.get('id'));
+              });
+              test('should have the property url_Photo', () => {
+                expect(['fake', 'real cert'])
+                  .toContain(WerkerCertification.get('url_Photo'));
+              });
+            });
+          });
+          describe('positions', () => {
+            let positions;
+            let position;
+            let WerkerPosition;
+            beforeAll(async () => {
+              positions = await updatedWerker.getPositions({
+                joinTableAttributes: ['WerkerId', 'PositionId'],
+              });
+              [position] = positions;
+              // eslint-disable-next-line prefer-destructuring
+              WerkerPosition = positions[0].WerkerPosition;
+            });
+            test('should exist', () => {
+              expect(positions[0]).toBeInstanceOf(dbHelpers.models.Position);
+            });
+            test('should have two positions', () => {
+              expect(positions.length).toEqual(1);
+            });
+            test('should have attribute position', () => {
+              expect(position.get('position')).toBe('dishwasher');
+            });
+            describe('WerkerPosition', () => {
+              test('should share WerkerId with werker', () => {
+                expect(WerkerPosition.get('WerkerId')).toEqual(newWerker.get('id'));
+              });
+              test('should share PositionId with position', () => {
+                expect(WerkerPosition.get('PositionId')).toEqual(position.get('id'));
+              });
+            });
+          });
+        });
       });
     });
   });
@@ -348,7 +275,7 @@ describe('Database', () => {
         expect(dbHelpers.createShift).toBeInstanceOf(Function);
       });
       test('should create a Shift', async () => {
-        const shiftTemplate = Object.assign(examples.Shift, newMaker.get('id'));
+        const shiftTemplate = Object.assign(examples.Shift, { MakerId: newMaker.get('id') });
         newShift = await dbHelpers.createShift(shiftTemplate);
         expect(newShift).toBeInstanceOf(dbHelpers.models.Shift);
       });
@@ -408,6 +335,61 @@ describe('Database', () => {
       test('should not fetch werkers if none can apply to shift based on positions', async () => {
         werkers = await dbHelpers.getWerkersForShift(newShift.get('id'));
         expect(werkers.length).toBe(0);
+      });
+    });
+    describe('applyOrInviteForShift', () => {
+      test('should exist', () => {
+        expect(dbHelpers.applyOrInviteForShift).toBeInstanceOf(Function);
+      });
+      test('should invite a werker to a shift', async () => {
+        const position = await dbHelpers.models.Position.findOne({
+          where: {
+            position: 'host',
+          },
+        });
+        const addedPosition = await newShift.setPositions(position, {
+          through: {
+            payment_type: 'cash',
+            payment_amnt: 20,
+          },
+        });
+        console.log(addedPosition);
+        const shift = await dbHelpers.models.Shift.findOne({
+          include: [
+            {
+              model: dbHelpers.models.Position,
+              where: {
+                position: 'host',
+              },
+            },
+          ],
+        });
+        console.log(shift);
+        // const shiftPosition = await dbHelpers.models.Position.findOne({
+        //   attributes: ['position'],
+        //   include: [
+        //     {
+        //       model: dbHelpers.models.Shift,
+        //       through: {
+        //         model: dbHelpers.models.ShiftPosition,
+        //         attributes: ['filled', 'payment_type', 'payment_amnt'],
+        //         where: {
+        //           ShiftId: newShift.get('id'),
+        //         },
+        //       },
+        //       attributes: ['id', 'name'],
+        //     },
+        //   ],
+        //   where: {
+        //     position: 'host',
+        //   },
+        // });
+        // console.log(shiftPosition);
+        const otherWerker = await dbHelpers.addWerker(examples.otherWerker);
+        console.log(newShift.get('id'));
+        const invitation = await dbHelpers.applyOrInviteForShift(newShift.get('id'), otherWerker.get('id'), 'host', 'invite');
+        console.log(invitation);
+        expect(invitation).toBeDefined();
       });
     });
   });
